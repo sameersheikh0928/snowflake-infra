bb60c59be9114fce067d78a5f2a2aa6306295026
sameersheikh0928 <sameersheikh0928@gmail.com>
2026-08-02 15:17:09 -0500

    Fix variables.tf syntax

--- variables.tf
+++ variables.tf
@@ -1,5 +1,21 @@
-variable "snowflake_account"     { type = string }
-variable "snowflake_org"         { type = string }
-variable "snowflake_user"        { type = string }
-variable "snowflake_private_key" { type = string, sensitive = true }
-variable "aws_region"            { type = string, default = "us-east-2" }
+variable "snowflake_account" {
+  type = string
+}
+
+variable "snowflake_org" {
+  type = string
+}
+
+variable "snowflake_user" {
+  type = string
+}
+
+variable "snowflake_private_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
